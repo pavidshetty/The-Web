fc65c6bf731255c4c229f541954a788349c72bc7
pavidshetty <pavitrashetty23@gmail.com>
2019-11-14 17:16:18 +0000

    using forms

--- Documents/Web/app.rb
+++ Documents/Web/app.rb
@@ -10,3 +10,6 @@ get '/named-cat' do
   @name = params[:name] # you need to add the query string(name) on the path
   erb(:index)
 end
+
+#form element has two attributes action- defines the location the data to be sent
+# to and method efines which HTTP method to send the data with (it can be "get" or "post
