100401693ed7686033d69e0f331f052d94b1b7d7
pavidshetty <pavitrashetty23@gmail.com>
2019-11-14 16:17:29 +0000

    params added

--- Documents/Web/app.rb
+++ Documents/Web/app.rb
@@ -1,5 +1,12 @@
 require 'sinatra'
 
-get '/cat' do
+get '/random-cat' do
+  @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params # prints the paramter on the terminal
+  @name = params[:name] # you need to add the query string(name) on the path
   erb(:index)
 end
