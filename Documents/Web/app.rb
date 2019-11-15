d8506d1462378fa6964c756443b5e38a965b9619
pavidshetty <pavitrashetty23@gmail.com>
2019-11-15 16:53:57 +0000

    sending params via post method

--- Documents/Web/app.rb
+++ Documents/Web/app.rb
@@ -5,11 +5,12 @@ get '/random-cat' do
   erb(:index)
 end
 
-get '/named-cat' do
+post '/named-cat' do
   p params # prints the paramter on the terminal
   @name = params[:name] # you need to add the query string(name) on the path
   erb(:index)
 end
 
-#form element has two attributes action- defines the location the data to be sent
-# to and method efines which HTTP method to send the data with (it can be "get" or "post
+get '/caty' do
+  erb(:cat_form)
+end
